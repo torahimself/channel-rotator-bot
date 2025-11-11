07fcb759295c197a02118dcda878917813f6c0ce
torahimself <torahimself@gmail.com>
2025-11-11 20:39:52 +0800

    Update eventHandler.js

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 
+function loadEvents(client) {
+  const eventsPath = path.join(__dirname, '../events');
+  const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+  for (const file of eventFiles) {
+    const event = require(path.join(eventsPath, file));
+    
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args));
+    }
+  }
+
+  console.log(`✅ Loaded ${eventFiles.length} events`);
+}
+
+module.exports = { loadEvents };
