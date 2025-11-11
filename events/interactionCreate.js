209a8798bb306c918cbd57f53237d81f1549ff60
torahimself <torahimself@gmail.com>
2025-11-11 20:40:26 +0800

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,10 @@
+const commandHandler = require('../handlers/commandHandler');
 
+module.exports = {
+  name: 'interactionCreate',
+  async execute(interaction) {
+    if (interaction.isCommand()) {
+      commandHandler.executeCommand(interaction);
+    }
+  },
+};
