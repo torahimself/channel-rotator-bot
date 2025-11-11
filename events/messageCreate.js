dc5462faf106b1c2526d73ad88cd3614f6c60027
torahimself <torahimself@gmail.com>
2025-11-11 23:09:18 +0800

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,7 +2,6 @@ module.exports = {
   name: 'messageCreate',
   async execute(message) {
     // This event is needed for the interaction system to work
-    // Most logic is handled in interactionCreate.js
     return;
   },
 };
