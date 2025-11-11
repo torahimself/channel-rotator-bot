b6732a40946810773e5596473a063d43751a2043
torahimself <torahimself@gmail.com>
2025-11-11 21:54:09 +0800

    Create messageCreate.js

---
+++ events/messageCreate.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'messageCreate',
+  async execute(message) {
+    // This event is needed for the interaction system to work
+    // Most logic is handled in interactionCreate.js
+    return;
+  },
+};
